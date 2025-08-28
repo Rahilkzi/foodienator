d054d649827b7512d22b0300628df85a7135740d
Rahil <rahilkazi66@gmail.com>
2025-08-28 18:03:17 +0530

    fix checkout

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -2,10 +2,6 @@
 defined('BASEPATH') OR exit ('No direct script access allowed');
 
 class Checkout extends CI_Controller {
-    
-
-
- 
     function __construct() {
         parent::__construct();
 
